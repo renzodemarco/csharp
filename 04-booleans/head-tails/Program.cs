4d27d467eeafcf8c8390d3ec44ce74e262187491
Renzo <renzo11demarco@gmail.com>
2025-03-26 17:51:09 -0300

    heads and tails practice

---
+++ 04-booleans/head-tails/Program.cs
@@ -0,0 +1,6 @@
+﻿Random coin = new Random();
+
+int number = coin.Next(0, 9);
+string coinSide = number >= 5 ? "heads" : "tails";
+
+Console.WriteLine($"Number is {number} so the coin landed on {coinSide}");
