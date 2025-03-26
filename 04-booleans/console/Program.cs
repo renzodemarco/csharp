14d9a23f27ea96f3a66888b49ccb031a9c3bd2bc
Renzo <renzo11demarco@gmail.com>
2025-03-26 17:39:02 -0300

    practice

--- 04-booleans/console/Program.cs
+++ 04-booleans/console/Program.cs
@@ -1,3 +1,7 @@
-﻿string value1 = " a";
-string value2 = "A ";
-Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+﻿// string value1 = " a";
+// string value2 = "A ";
+// Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+
+string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(pangram.Contains("fox"));
+Console.WriteLine(!pangram.Contains("cow"));
--- 04-booleans/console/obj/Debug/net9.0/console.AssemblyInfo.cs
+++ 04-booleans/console/obj/Debug/net9.0/console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a414613bf30f93f471dc1e07b0fdf18e70bf1247")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf694144faf5f60e996e965cd114db080123e765")]
 [assembly: System.Reflection.AssemblyProductAttribute("console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
