9853b2ae36b927c1ebd09765b3c8d86445b749e5
Renzo <renzo11demarco@gmail.com>
2025-03-30 17:20:23 -0300

    boolean practice

---
+++ 04-booleans/admin/Program.cs
@@ -0,0 +1,17 @@
+﻿string permission = "Admin|Manager";
+int level = 95;
+
+if (permission.Contains("Admin")) 
+{
+  string msg = level > 55 ? "Welcome Super Admin user." : "Welcome Admin user.";
+  Console.WriteLine(msg);
+}
+else if (permission.Contains("Manager"))
+{
+  string msg = level > 20 ? "Contact an Admin for access." : "You do not have sufficient privileges.";
+  Console.WriteLine(msg);
+}
+else 
+{
+Console.WriteLine("You do not have sufficient privileges.");
+}
