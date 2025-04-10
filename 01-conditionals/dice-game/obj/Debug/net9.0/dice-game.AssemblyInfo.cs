165d66ac5e59b8dae622861d07de48f8ba29c5b4
Renzo <renzo11demarco@gmail.com>
2025-04-10 18:10:48 -0300

    practice

--- 01-conditionals/dice-game/obj/Debug/net9.0/dice-game.AssemblyInfo.cs
+++ 01-conditionals/dice-game/obj/Debug/net9.0/dice-game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dice-game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35bde0e5b7214b7915b539f65ea9a0ccce2b9898")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fc70e5896c4ae9d321d86114f1e70486c3ea72d")]
 [assembly: System.Reflection.AssemblyProductAttribute("dice-game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dice-game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
