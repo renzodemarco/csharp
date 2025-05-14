402ba17f80036cfdfbedf31278afb67ea24d7b74
Renzo <renzo11demarco@gmail.com>
2025-05-14 10:38:09 -0300

    practice

--- 01-conditionals/dice-game/obj/Debug/net9.0/dice-game.AssemblyInfo.cs
+++ 01-conditionals/dice-game/obj/Debug/net9.0/dice-game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dice-game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+992525ac11b300b6002cf779434c4288b9de02c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcc0fae9d1559822b764267ab5045bba078396d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("dice-game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dice-game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- 01-conditionals/subscription/obj/Debug/net9.0/subscription.AssemblyInfo.cs
+++ 01-conditionals/subscription/obj/Debug/net9.0/subscription.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("subscription")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+992525ac11b300b6002cf779434c4288b9de02c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcc0fae9d1559822b764267ab5045bba078396d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("subscription")]
 [assembly: System.Reflection.AssemblyTitleAttribute("subscription")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
