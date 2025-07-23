31b408f887169d49d2eed8da451f4f813314d317
Renzo <renzo11demarco@gmail.com>
2025-07-23 18:08:59 -0300

    practice

--- 01-conditionals/subscription/obj/Debug/net9.0/subscription.AssemblyInfo.cs
+++ 01-conditionals/subscription/obj/Debug/net9.0/subscription.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("subscription")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+750d4fe9c019d43632ca330e622c381137d1d148")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+620e5ac8295437f1803a5eae2558bfbd971b855a")]
 [assembly: System.Reflection.AssemblyProductAttribute("subscription")]
 [assembly: System.Reflection.AssemblyTitleAttribute("subscription")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
